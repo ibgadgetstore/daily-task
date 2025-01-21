b3e01febe92ffc384dd71117b941b3b3829408aa
IBCORP <asistenseoibcorp@gmail.com>
2025-01-21 17:12:41 +0700

    Add files via upload

---
+++ framerusercontent.com/sites/3cXGcpdiDU0Q3vtWTqHhYN/chunk-DAYTG3JC.mjs
@@ -0,0 +1,2 @@
+function a(t,e){return{description:"Ayo kerjakan tugas kamu sekarang!",favicon:"https://framerusercontent.com/assets/uLPBEZzPgv9QT56ufhUvcz8HKI.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/uLPBEZzPgv9QT56ufhUvcz8HKI.png",title:"Daily Task"}}export{a};
+//# sourceMappingURL=chunk-DAYTG3JC.mjs.map
